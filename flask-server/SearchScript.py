04280106af542b285569aad6919ab05a0ae4a366
Izaiah-Caston <donutzay@gmail.com>
2024-07-23 15:06:58 -0500

    Commit Message

--- flask-server/SearchScript.py
+++ flask-server/SearchScript.py
@@ -1,18 +1,48 @@
-import argparse
-import sys 
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from pytube import YouTube
 from pytube import Search
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime
 
 
-AUDIO_DOWNLOAD_DIR = r"C:\Users\donut\Music"
-urls = []
-videoUrl = ''
 
+#Create a Flask Instance
 app = Flask(__name__)
 CORS(app)
 
+#Add Database
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://SearchScript.db'
+#Key
+app.config['SECRET_KEY'] = "my key"
+#DB intialization
+db.init_app(app)
+
+#Create Model
+class Songs(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(50), nullable = False, unique = True)
+    ## genre = db.Column(db.String(50), nullable = False)
+    ## song_length = db.Column(db.Integer, nullable = False)
+    ## artist = db.Column(db.String(50), nullable = False)
+    date_added = db.Column(db.DateTime, default = datetime.utcnow)
+    
+    #Create a String
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+#Variables for the script
+AUDIO_DOWNLOAD_DIR = db
+urls = []
+videoUrl = ''
+
+#Main Script Code
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -35,4 +65,5 @@ def YoutubeAudioDownload():
         print(f"Failed to download: {e}")
 
 if __name__ == "__main__":
- app.run(debug=True)
+    db.create_all()
+    app.run(debug=True)
